a4649798b08e08c9960e2a0c807dd7ba456cf647
Calupoh <m.moreno@calupog.com>
2022-10-26 16:28:33 -0500

    ajustes visuales a #b-meu

--- b-menu.js
+++ b-menu.js
@@ -6,9 +6,14 @@ function show_menu() {
     x = pop_menu.style.display;
     if ( x == 'none' || x == '') {
         pop_menu.style.display = 'flex';
+        pop_menu.style['flex-direction'] = 'column';
+        b_menu.style.background = '#0e3296cb';
+        b_menu.style.color = 'aliceblue';
     }
     else {
         pop_menu.style.display = 'none';
+        b_menu.style.background = 'none';
+        b_menu.style.color = 'black';
     }
 }
 
